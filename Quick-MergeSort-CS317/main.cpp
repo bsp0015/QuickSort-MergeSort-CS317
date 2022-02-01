7a3a3a23aa6c23a374be6260c157dc256abc8f49
Brandon Perry <bsp0015@uah.edu>
2022-02-01 12:45:57 -0600

    Initial Commit

---
+++ Quick-MergeSort-CS317/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Quick-MergeSort-CS317
+//
+//  Created by Brandon Perry on 2/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
